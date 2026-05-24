389413d7d00cf5accc4e5a90662a6d54d42de998
alexxware <manuelmorm1@gmail.com>
2026-05-23 20:51:54 -0600

    simple fetching pokemons

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
 
-export default function PokemosPage() {
+const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    .then(res => res.json())
+    .catch(res => res.json());
+
+  const pokemons = data.results.map(pokemon => ({
+    id: pokemon.url.split('/').at(-2)!,
+    name: pokemon.name,
+  }))
+
+  return pokemons;
+}
+
+export default async function PokemosPage() {
+  const pokemons = await getPokemons(151);
   return (
-    <div>
-      Pokemos
+    <div className="flex flex-col">
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        {
+          pokemons.map(pokemon => (
+            <Image
+              key={pokemon.id}
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+              width={100}
+              height={100}
+              alt={pokemon.name} />
+          ))
+        }
+      </div>
     </div>
   )
 }
---
+++ app/pokemons/index.ts
@@ -0,0 +1,2 @@
+export type { SimplePokemon } from './interfaces/simple-pokemon';
+export type { PokemonsResponse } from './interfaces/pokemons-response';
